402c34c98e40c2fa2550aa58a51b8c81755869b1
Chani17 <cucute8878@naver.com>
2023-11-01 17:26:00 +0900

    Add: Add plusRate & minusRate in update sto round request dto

--- app/src/main/java/inu/thebite/tory/model/sto/UpdateStoRoundRequest.kt
+++ app/src/main/java/inu/thebite/tory/model/sto/UpdateStoRoundRequest.kt
@@ -4,6 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class UpdateStoRoundRequest(
 
+    @SerializedName("plusRate")
+    var plusRate : Float,
+
+    @SerializedName("minusRate")
+    var minusRate : Float,
+
     @SerializedName("registrant")
     var registrant : String
 )
